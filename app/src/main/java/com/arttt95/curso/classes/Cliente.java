6da8d73b13825b40aff85c631e0a56b13b45689b
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-11-27 20:01:23 -0300

    27NOV24 - Aula 43 AS - Modificadores de acesso.

---
+++ app/src/main/java/com/arttt95/curso/classes/Cliente.java
@@ -0,0 +1,9 @@
+package com.arttt95.curso.classes;
+
+public class Cliente {
+
+    void teste(){
+        ContaBancaria conta1 = new ContaBancaria();
+    }
+
+}
---
+++ app/src/main/java/com/arttt95/curso/classes/ContaBancaria.java
@@ -0,0 +1,32 @@
+package com.arttt95.curso.classes;
+
+public class ContaBancaria {
+
+    // Atributos
+    private double saldo = 0;
+
+    // Métodos
+    public void sacar(){
+        // Recuperar o saldo atual da pessoa
+        this.saldo = 100;
+        this.recuperarSaldo();
+    }
+
+    public void transferir(){
+        // Recuperar o saldo atual da pessoa
+    }
+
+    private void recuperarSaldo() {
+        // Lógica
+    }
+
+
+}
+
+// Default -> Dentro da própria classe e do pacote
+
+// Public -> Em qualquer lugar do código
+
+// Private -> Apenas dentro da própria classe
+
+// Protected -> Dentro do mesmo pacote e/ou dentro das subclasses
